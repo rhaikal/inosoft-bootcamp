4b27adbb5fc4ab40154bfac628af90c2d80a78a8
rhaikal <rhaikal101@gmail.com>
2022-10-11 11:06:20 +0700

    mengubah routes task  dari web ke api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controller\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('task')->group(function() {
+    Route::post('/show_tasks', [TaskController::class, 'showTasks']);
+    Route::post('/create_task', [TaskController::class, 'createTask']);
+    Route::post('/update_task', [TaskController::class, 'updateTask']);
+
+    // NOTE: lanjutkan tugas assignment di routing baru dibawah ini
+    Route::post('/delete_task', [TaskController::class, 'deleteTask']);
+    Route::post('/assign_task', [TaskController::class, 'assignTask']);
+    Route::post('/unassign_task', [TaskController::class, 'unassignTask']);
+    Route::post('/create_subtask', [TaskController::class, 'createSubtask']);
+    Route::post('/delete_subtask', [TaskController::class, 'deleteSubtask']);
 });
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controller\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,15 +13,3 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('task')->group(function() {
-    Route::post('/show_tasks', [TaskController::class, 'showTasks']);
-    Route::post('/create_task', [TaskController::class, 'createTask']);
-    Route::post('/update_task', [TaskController::class, 'updateTask']);
-
-    // NOTE: lanjutkan tugas assignment di routing baru dibawah ini
-    Route::post('/delete_task', [TaskController::class, 'deleteTask']);
-    Route::post('/assign_task', [TaskController::class, 'assignTask']);
-    Route::post('/unassign_task', [TaskController::class, 'unassignTask']);
-    Route::post('/create_subtask', [TaskController::class, 'createSubtask']);
-    Route::post('/delete_subtask', [TaskController::class, 'deleteSubtask']);
-});
